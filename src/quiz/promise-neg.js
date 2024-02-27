4be64e3d8a0035cdb3b97a43479d33abe6a8ed24
Cheng Shi <sc971008@gmail.com>
2024-02-27 13:25:06 -0500

    work in progress

--- src/quiz/promise-neg.js
+++ src/quiz/promise-neg.js
@@ -3,3 +3,35 @@ const array2D = [
     [4, 5, 6],
     [7, 8, -9]
 ];
+
+function checkNegArray(arr){
+    return new Promise((resolve,reject) =>{
+        
+        if(Array.isArray(arr)){
+            for (let i = 0; i < arr.length; i++) {
+                    if(arr[i]<0) resolve(`Found Neg ${arr[i]}`);
+            }
+            console.log('resolving ... '+sum);
+            reject("all positive")
+        }
+        else {
+            console.log('rejecting ... ');
+            reject('BAD INPUT: Expected array as input');
+        }
+        console.log('returning from check');
+    })
+}
+
+
+const Promises = []
+for(let i= 0 ; i<array2D.length ;i++){
+    Promises.push(checkNegArray(array2D[i]));
+}
+
+Promise.any(Promises)
+    .then((res)=>{
+        console(res)
+    })
+    .catch((error) => {
+        console.error(`Failed to fetch: ${error}`);
+    });
--- src/quiz/promise-sum-2darray.js
+++ src/quiz/promise-sum-2darray.js
@@ -3,3 +3,40 @@ const array2D = [
     [4, 5, 6],
     [7, 8, 9]
 ];
+
+function sumArray(arr){
+    return new Promise((resolve,reject) =>{
+        console.log("1D sum is called")
+        if(Array.isArray(arr)){
+            let sum = 0;
+            for (let i = 0; i < arr.length; i++) {
+                    sum += arr[i];
+            }
+            console.log('resolving ... '+sum);
+            resolve(sum);
+        }
+        else {
+            console.log('rejecting ... ');
+            reject('BAD INPUT: Expected array as input');
+        }
+        console.log('returning from sum');
+    })
+}
+
+const Promises = []
+for(let i= 0 ; i<array2D.length ;i++){
+    Promises.push(sumArray(array2D[i]));
+}
+
+
+Promise.all(Promises)
+    .then((res)=>{
+        let Ans =0
+        res.forEach(element => {
+            Ans= Ans+element
+        });
+        console.log(Ans)
+    })
+    .catch((error) => {
+        console.error(`Failed to fetch: ${error}`);
+    });
