c79937cfdb47c4e5886b039dd4409517907226ff
Cheng Shi <sc971008@gmail.com>
2024-03-03 14:22:15 -0500

    updated

--- src/quiz/promise-neg.js
+++ src/quiz/promise-neg.js
@@ -1,18 +1,17 @@
 const array2D = [
     [1, 2, 3],
     [4, 5, 6],
-    [7, 8, -9]
+    [7, 8, 9]
 ];
 
 function checkNegArray(arr){
     return new Promise((resolve,reject) =>{
-        
         if(Array.isArray(arr)){
             for (let i = 0; i < arr.length; i++) {
                     if(arr[i]<0) resolve(`Found Neg ${arr[i]}`);
             }
-            console.log('resolving ... '+sum);
-            reject("all positive")
+            console.log('rejecting ...');
+            reject("1D array all positive")
         }
         else {
             console.log('rejecting ... ');
@@ -30,7 +29,7 @@ for(let i= 0 ; i<array2D.length ;i++){
 
 Promise.any(Promises)
     .then((res)=>{
-        console(res)
+        console.log(res)
     })
     .catch((error) => {
         console.error(`Failed to fetch: ${error}`);
